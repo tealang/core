914f20c70df505a7d93862f84e2651a29b500e83
Lennart Espe <lennart@espe.tech>
2017-09-07 21:00:00 +0200

    Improve runtime/context segment quality

--- tea/runtime/context.go
+++ tea/runtime/context.go
@@ -1,19 +1,36 @@
+// Package runtime provides the Tealang runtime functionality including namespaces, context and execution nodes.
 package runtime
 
+// ContextBehavior controls the runtime execution behavior.
 type ContextBehavior int
 
 const (
+	// BehaviorDefault is the normal behavior.
 	BehaviorDefault ContextBehavior = iota
 )
 
+// Context is the runtime context the AST is executed in.
 type Context struct {
 	Namespace       *Namespace
 	GlobalNamespace *Namespace
 	Behavior        ContextBehavior
 }
 
+// Substitute executes the method in a substituted namespace.
+// This makes new variables within the substituted namespace unavailable to everyone outside of it.
 func (c *Context) Substitute(f func(*Context) (Value, error)) (Value, error) {
 	backup := c.Namespace
+	c.Namespace = NewNamespace(c.Namespace)
 	defer func() { c.Namespace = backup }()
 	return f(c)
+}
+
+// NewContext instantiates a new context with only one namespace, both local and global.
+func NewContext() *Context {
+	ns := NewNamespace(nil)
+	return &Context{
+		Namespace:       ns,
+		GlobalNamespace: ns,
+		Behavior:        BehaviorDefault,
+	}
 }
---
+++ tea/runtime/context_test.go
@@ -0,0 +1,34 @@
+package runtime
+
+import (
+	"testing"
+)
+
+func TestNewContext(t *testing.T) {
+	ctx := NewContext()
+	if ctx.GlobalNamespace != ctx.Namespace {
+		t.Error("Global and local namespace should be equal")
+	}
+	if ctx.Behavior != BehaviorDefault {
+		t.Error("Behavior on New should be default")
+	}
+}
+
+func TestContext_Substitute(t *testing.T) {
+	ctx := NewContext()
+	v := Value{Name: "v"}
+	if x, err := ctx.Substitute(func(c *Context) (Value, error) {
+		c.Namespace.Store(v)
+		return v, nil
+	}); err != nil {
+		t.Errorf("Unexpected error on substitute: %s", err)
+	} else if x != v {
+		t.Errorf("Invalid value: expected %v, got %v", v, x)
+	}
+	if _, err := ctx.Namespace.Find(IdentifierSearchSpace, "v"); err == nil {
+		t.Errorf("Expected error on find: %s", err)
+	}
+	if _, err := ctx.GlobalNamespace.Find(IdentifierSearchSpace, "v"); err == nil {
+		t.Errorf("Expected error on find (global): %s", err)
+	}
+}
