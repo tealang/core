19fb30c673adfd450f7b910c1b555d134d6da854
Lennart Espe <lennart@espe.tech>
2017-09-11 20:10:27 +0200

    Add loop tree node

---
+++ tea/runtime/nodes/loop.go
@@ -0,0 +1,32 @@
+package nodes
+
+import "github.com/tealang/tea-go/tea/runtime"
+
+type Loop struct {
+	Conditional
+}
+
+func (Loop) Name() string {
+	return "Loop"
+}
+
+func (l *Loop) Eval(c *runtime.Context) (runtime.Value, error) {
+	value, err := l.Conditional.Eval(c)
+	_, ok := err.(ConditionalException)
+	for !ok {
+		switch c.Behavior {
+		case runtime.BehaviorReturn:
+			return value, nil
+		case runtime.BehaviorBreak:
+			return runtime.Value{}, nil
+		default:
+			value, err = l.Conditional.Eval(c)
+		}
+	}
+	return value, nil
+}
+
+func NewLoop(condition, body Node) *Loop {
+	cond := NewConditional(condition, body)
+	return &Loop{*cond}
+}
