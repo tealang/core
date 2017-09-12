5f3db0edb87187b369aeb354f4e1d8d5c07d2610
Lennart Espe <lennart@espe.tech>
2017-09-12 21:34:48 +0200

    Add controller tree node

---
+++ tea/runtime/nodes/controller.go
@@ -0,0 +1,25 @@
+package nodes
+
+import "github.com/tealang/tea-go/tea/runtime"
+
+// Controller executes a range of statements and modifies the current context behavior before returning..
+type Controller struct {
+	BasicNode
+	Behavior runtime.ContextBehavior
+}
+
+func (Controller) Name() string {
+	return "Controller"
+}
+
+func (ctrl *Controller) Eval(c *runtime.Context) (runtime.Value, error) {
+	var value runtime.Value
+	for _, n := range ctrl.Childs {
+		value, err := n.Eval(c)
+		if err != nil {
+			return value, err
+		}
+	}
+	c.Behavior = ctrl.Behavior
+	return value, nil
+}
