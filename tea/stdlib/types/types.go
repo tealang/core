103832c7046fba868934a22a5648548868eabde0
Lennart Espe <lennart@espe.tech>
2017-09-15 19:13:40 +0200

    Add Caster and Formatter to types Any, Bool

--- tea/stdlib/types/types.go
+++ tea/stdlib/types/types.go
@@ -1,16 +1,47 @@
 package types
 
-import "github.com/tealang/tea-go/tea/runtime"
+import (
+	"fmt"
+
+	"github.com/tealang/tea-go/tea/runtime"
+)
 
 var (
+	Any, Bool *runtime.Datatype
+)
+
+func init() {
 	Any = &runtime.Datatype{
-		Name: "any",
+		Name:   "any",
+		Parent: nil,
+		Cast: func(v runtime.Value) (runtime.Value, error) {
+			return runtime.Value{
+				Type: Any,
+				Data: v,
+			}, nil
+		},
+		Format: func(v runtime.Value) string {
+			v, ok := v.Data.(runtime.Value)
+			if !ok {
+				return "any<?>"
+			}
+			return fmt.Sprintf("any<%s>", v.Type.Format(v))
+		},
 	}
 	Bool = &runtime.Datatype{
 		Name:   "bool",
 		Parent: Any,
+		Cast: func(v runtime.Value) (runtime.Value, error) {
+			if v.Type == Bool {
+				return v, nil
+			}
+			return runtime.Value{}, runtime.ExplicitCastException{From: v.Type, To: Bool}
+		},
+		Format: func(v runtime.Value) string {
+			return fmt.Sprintf("bool<%s>", v.Data)
+		},
 	}
-)
+}
 
 var (
 	True = runtime.Value{
