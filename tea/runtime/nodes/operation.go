21a69b3411f5822c9f9ae7d7e96aa91d19e9505f
Lennart Espe <lennart@espe.tech>
2017-09-16 00:45:01 +0200

    Add operation tree node

---
+++ tea/runtime/nodes/operation.go
@@ -0,0 +1,33 @@
+package nodes
+
+import "github.com/tealang/tea-go/tea/runtime"
+
+// Operation calls an operator on the results of its children as arguments.
+type Operation struct {
+	BasicNode
+	Symbol string
+}
+
+func (Operation) Name() string {
+	return "Operation"
+}
+
+func (o *Operation) Eval(c *runtime.Context) (runtime.Value, error) {
+	item, err := c.Namespace.Find(runtime.SearchOperator, o.Symbol)
+	if err != nil {
+		return runtime.Value{}, err
+	}
+	op, ok := item.(runtime.Operator)
+	if !ok {
+		return runtime.Value{}, runtime.UnexpectedItemException{Expected: runtime.Operator{}, Got: item}
+	}
+	args := make([]runtime.Value, len(o.Childs))
+	for i, n := range o.Childs {
+		v, err := n.Eval(c)
+		if err != nil {
+			return runtime.Value{}, err
+		}
+		args[i] = v
+	}
+	return op.Eval(args, c)
+}
