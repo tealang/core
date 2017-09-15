45e162f202362c7b9a358dc6fd747d366c81d703
Lennart Espe <lennart@espe.tech>
2017-09-15 18:21:16 +0200

    Add Adapter tree node

---
+++ tea/runtime/nodes/adapter.go
@@ -0,0 +1,24 @@
+package nodes
+
+import "github.com/tealang/tea-go/tea/runtime"
+
+// Adapter allows a Go function to be used as a tree node.
+type Adapter struct {
+	BasicNode
+	Func func(c *runtime.Context) (runtime.Value, error)
+}
+
+func (Adapter) Name() string {
+	return "Adapter"
+}
+
+func (a *Adapter) Eval(c *runtime.Context) (runtime.Value, error) {
+	return a.Func(c)
+}
+
+func NewAdapter(f func(c *runtime.Context) (runtime.Value, error)) *Adapter {
+	return &Adapter{
+		BasicNode: NewBasic(),
+		Func:      f,
+	}
+}
