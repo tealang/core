75f54c779a05bf37a68d8afa821b6eafcc9f969e
Lennart Espe <lennart@espe.tech>
2017-09-16 16:32:17 +0200

    Add Assignment node

---
+++ tea/runtime/nodes/assignment.go
@@ -0,0 +1,40 @@
+package nodes
+
+import "github.com/tealang/tea-go/tea/runtime"
+
+// Assignment assigns one or multiple existing variables a new value (for each variable).
+type Assignment struct {
+	BasicNode
+	Alias []string
+}
+
+func (Assignment) Name() string {
+	return "Assignment"
+}
+
+func (a *Assignment) Eval(c *runtime.Context) (runtime.Value, error) {
+	if len(a.Childs) != len(a.Alias) {
+		return runtime.Value{}, runtime.AssignmentMismatchException{}
+	}
+	var (
+		value runtime.Value
+		err   error
+	)
+	for i, n := range a.Childs {
+		value, err = n.Eval(c)
+		if err != nil {
+			return runtime.Value{}, err
+		}
+		if err = c.Namespace.Update(value.Rename(a.Alias[i])); err != nil {
+			return runtime.Value{}, err
+		}
+	}
+	return value, nil
+}
+
+func NewAssignment(alias []string, values ...Node) *Assignment {
+	return &Assignment{
+		BasicNode: NewBasic(values...),
+		Alias:     alias,
+	}
+}
