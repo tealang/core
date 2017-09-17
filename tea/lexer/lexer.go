3eadb2bfb49c4db9cb9a94bcdcd7ed09901c572e
Lennart Espe <lennart@espe.tech>
2017-09-17 20:15:54 +0200

    Split up lexer and tokens

--- tea/lexer/lexer.go
+++ tea/lexer/lexer.go
@@ -1,104 +1,13 @@
 package lexer
 
-import (
-	"fmt"
-	"regexp"
-)
+import "github.com/tealang/tea-go/tea/lexer/tokens"
 
-type TokenMatcher func(s string) bool
-
-func NewTokenMatcher(expr string) TokenMatcher {
-	regex, err := regexp.Compile(expr)
-	if err != nil {
-		panic(err)
-	}
-	return func(s string) bool {
-		return regex.Match([]byte(s))
-	}
-}
-
-type TokenType struct {
-	Name  string
-	Match TokenMatcher
-}
-
-func (tt TokenType) String() string {
-	return tt.Name
-}
-
-type Token struct {
-	Type  *TokenType
-	Value string
-}
-
-func (t Token) String() string {
-	return fmt.Sprintf("%s('%s')", t.Type, t.Value)
-}
-
-var (
-	AllTypes = []TokenType{
-		{
-			Name:  "leftParentheses",
-			Match: NewTokenMatcher(`^\($`),
-		},
-		{
-			Name:  "rightParentheses",
-			Match: NewTokenMatcher(`^\)$`),
-		},
-		{
-			Name:  "operator",
-			Match: NewTokenMatcher(`^([+\-*/=:<>!%^&|]|([+\-*/^%<>=!]=)|([|^]\|)|(&&))$`),
-		},
-		{
-			Name:  "whitespace",
-			Match: NewTokenMatcher(`^\s+$`),
-		},
-		{
-			Name:  "number",
-			Match: NewTokenMatcher(`^\-?[0-9]+(\.[0-9]*)?$`),
-		},
-		{
-			Name:  "identifier",
-			Match: NewTokenMatcher(`^(#|[a-zA-Z_])+([0-9a-zA-Z_]+)?$`),
-		},
-		{
-			Name:  "string",
-			Match: NewTokenMatcher(`^"(\\(["abfnrtv])?|[^\n\r"])*"?$`),
-		},
-		{
-			Name:  "statement",
-			Match: NewTokenMatcher(`^;$`),
-		},
-		{
-			Name:  "separator",
-			Match: NewTokenMatcher(`^,$`),
-		},
-		{
-			Name:  "rightBlock",
-			Match: NewTokenMatcher(`^}$`),
-		},
-		{
-			Name:  "leftBlock",
-			Match: NewTokenMatcher(`^{$`),
-		},
-	}
-)
-
-func FindMatch(value string) *TokenType {
-	for _, tt := range AllTypes {
-		if tt.Match(value) {
-			return &tt
-		}
-	}
-	return nil
-}
-
-func Lex(input string) []Token {
-	active := Token{
+func Lex(input string) []tokens.Token {
+	active := tokens.Token{
 		Value: "",
 		Type:  nil,
 	}
-	output := []Token{}
+	output := []tokens.Token{}
 	for _, c := range input {
 		value := active.Value + string(c)
 		if active.Type != nil && active.Type.Match(value) {
@@ -107,9 +16,9 @@ func Lex(input string) []Token {
 			if active.Type != nil {
 				output = append(output, active)
 			}
-			active = Token{
+			active = tokens.Token{
 				Value: string(c),
-				Type:  FindMatch(string(c)),
+				Type:  tokens.FindMatch(string(c)),
 			}
 		}
 	}
---
+++ tea/lexer/tokens/tokens.go
@@ -0,0 +1,104 @@
+package tokens
+
+import (
+	"fmt"
+	"regexp"
+)
+
+type TokenMatcher func(s string) bool
+
+func NewTokenMatcher(expr string) TokenMatcher {
+	regex, err := regexp.Compile(expr)
+	if err != nil {
+		panic(err)
+	}
+	return func(s string) bool {
+		return regex.Match([]byte(s))
+	}
+}
+
+type TokenType struct {
+	Name  string
+	Match TokenMatcher
+}
+
+func (tt TokenType) String() string {
+	return tt.Name
+}
+
+type Token struct {
+	Type  *TokenType
+	Value string
+}
+
+func (t Token) String() string {
+	return fmt.Sprintf("%s('%s')", t.Type, t.Value)
+}
+
+var (
+	LeftParentheses = TokenType{
+		Name:  "leftParentheses",
+		Match: NewTokenMatcher(`^\($`),
+	}
+	RightParentheses = TokenType{
+		Name:  "rightParentheses",
+		Match: NewTokenMatcher(`^\)$`),
+	}
+	Operator = TokenType{
+		Name:  "operator",
+		Match: NewTokenMatcher(`^([+\-*/=:<>!%^&|]|([+\-*/^%<>=!]=)|([|^]\|)|(&&))$`),
+	}
+	Whitespace = TokenType{
+		Name:  "whitespace",
+		Match: NewTokenMatcher(`^\s+$`),
+	}
+	Number = TokenType{
+		Name:  "number",
+		Match: NewTokenMatcher(`^\-?[0-9]+(\.[0-9]*)?$`),
+	}
+	Identifier = TokenType{
+		Name:  "identifier",
+		Match: NewTokenMatcher(`^(#|[a-zA-Z_])+([0-9a-zA-Z_]+)?$`),
+	}
+	String = TokenType{
+		Name:  "string",
+		Match: NewTokenMatcher(`^"(\\(["abfnrtv])?|[^\n\r"])*"?$`),
+	}
+	Statement = TokenType{
+		Name:  "statement",
+		Match: NewTokenMatcher(`^;$`),
+	}
+	Separator = TokenType{
+		Name:  "separator",
+		Match: NewTokenMatcher(`^,$`),
+	}
+	RightBlock = TokenType{
+		Name:  "rightBlock",
+		Match: NewTokenMatcher(`^}$`),
+	}
+	LeftBlock = TokenType{
+		Name:  "leftBlock",
+		Match: NewTokenMatcher(`^{$`),
+	}
+	AllTypes = []TokenType{
+		LeftParentheses,
+		RightParentheses,
+		Operator,
+		Whitespace,
+		Number,
+		Identifier,
+		String,
+		Statement,
+		RightBlock,
+		LeftBlock,
+	}
+)
+
+func FindMatch(value string) *TokenType {
+	for _, tt := range AllTypes {
+		if tt.Match(value) {
+			return &tt
+		}
+	}
+	return nil
+}
