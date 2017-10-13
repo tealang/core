918914aa0bbd17974686bcbd481506b0f6464f35
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-13 21:36:47 +0200

    Add support for cross-type basic math operations

--- runtime/operators/basicmath.go
+++ runtime/operators/basicmath.go
@@ -7,6 +7,39 @@ import (
 )
 
 func loadMultiplication(c *runtime.Context) {
+	adapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
+		var (
+			identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
+			identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
+			a         = identA.(runtime.Value)
+			b         = identB.(runtime.Value)
+			err       error
+		)
+		if a.Type == types.Float {
+			b, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		} else if b.Type == types.Float {
+			a, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		}
+		switch a.Type {
+		case types.Integer:
+			return runtime.Value{
+				Type: types.Integer,
+				Data: a.Data.(int64) * b.Data.(int64),
+			}, nil
+		case types.Float:
+			return runtime.Value{
+				Type: types.Float,
+				Data: a.Data.(float64) * b.Data.(float64),
+			}, nil
+		}
+		return runtime.Value{}, runtime.NewNotApplicableException("/", a.Type, b.Type)
+	})
 	mulFloat := runtime.Signature{
 		Expected: []runtime.Value{
 			{
@@ -20,19 +53,24 @@ func loadMultiplication(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			return runtime.Value{
-				Type: types.Float,
-				Data: a.Data.(float64) * b.Data.(float64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Float},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Float},
+	}
+	mulFloatInteger := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	mulInteger := runtime.Signature{
 		Expected: []runtime.Value{
@@ -47,24 +85,31 @@ func loadMultiplication(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			return runtime.Value{
-				Type: types.Integer,
-				Data: a.Data.(int64) * b.Data.(int64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Integer},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Integer},
+	}
+	mulIntegerFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	mulFunction := runtime.Function{
 		Signatures: []runtime.Signature{
 			mulInteger,
+			mulIntegerFloat,
 			mulFloat,
+			mulFloatInteger,
 		},
 		Source: nil,
 	}
@@ -76,6 +121,47 @@ func loadMultiplication(c *runtime.Context) {
 	c.Namespace.Store(mul)
 }
 func loadDivision(c *runtime.Context) {
+	adapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
+		var (
+			identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
+			identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
+			a         = identA.(runtime.Value)
+			b         = identB.(runtime.Value)
+			err       error
+		)
+		if a.Type == types.Float {
+			b, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		} else if b.Type == types.Float {
+			a, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		}
+		switch a.Type {
+		case types.Integer:
+			bv := b.Data.(int64)
+			if bv == 0 {
+				return runtime.Value{}, runtime.Exception{Message: "Cannot divide by 0"}
+			}
+			return runtime.Value{
+				Type: types.Integer,
+				Data: a.Data.(int64) / bv,
+			}, nil
+		case types.Float:
+			bv := b.Data.(float64)
+			if bv == 0 {
+				return runtime.Value{}, runtime.Exception{Message: "Cannot divide by 0"}
+			}
+			return runtime.Value{
+				Type: types.Float,
+				Data: a.Data.(float64) / bv,
+			}, nil
+		}
+		return runtime.Value{}, runtime.NewNotApplicableException("/", a.Type, b.Type)
+	})
 	divFloat := runtime.Signature{
 		Expected: []runtime.Value{
 			{
@@ -89,22 +175,24 @@ func loadDivision(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			if b.Data.(float64) == 0.0 {
-				return runtime.Value{}, runtime.Exception{Message: "Cannot divide by 0"}
-			}
-			return runtime.Value{
-				Type: types.Float,
-				Data: a.Data.(float64) / b.Data.(float64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Float},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Float},
+	}
+	divFloatInteger := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	divInteger := runtime.Signature{
 		Expected: []runtime.Value{
@@ -119,27 +207,31 @@ func loadDivision(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			if b.Data.(int64) == 0 {
-				return runtime.Value{}, runtime.Exception{Message: "Cannot divide by 0"}
-			}
-			return runtime.Value{
-				Type: types.Integer,
-				Data: a.Data.(int64) / b.Data.(int64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Integer},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Integer},
+	}
+	divIntegerFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	divFunction := runtime.Function{
 		Signatures: []runtime.Signature{
 			divInteger,
+			divIntegerFloat,
 			divFloat,
+			divFloatInteger,
 		},
 		Source: nil,
 	}
@@ -179,6 +271,39 @@ func loadAddition(c *runtime.Context) {
 		}),
 		Returns: runtime.Value{Type: types.String},
 	}
+	addAdapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
+		var (
+			identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
+			identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
+			a         = identA.(runtime.Value)
+			b         = identB.(runtime.Value)
+			err       error
+		)
+		if a.Type == types.Float {
+			b, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		} else if b.Type == types.Float {
+			a, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		}
+		switch a.Type {
+		case types.Integer:
+			return runtime.Value{
+				Type: types.Integer,
+				Data: a.Data.(int64) + b.Data.(int64),
+			}, nil
+		case types.Float:
+			return runtime.Value{
+				Type: types.Float,
+				Data: a.Data.(float64) + b.Data.(float64),
+			}, nil
+		}
+		return runtime.Value{}, runtime.NewNotApplicableException("/", a.Type, b.Type)
+	})
 	plusFloat := runtime.Signature{
 		Expected: []runtime.Value{
 			{
@@ -232,19 +357,24 @@ func loadAddition(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			return runtime.Value{
-				Type: types.Float,
-				Data: a.Data.(float64) + b.Data.(float64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Float},
+		Function: addAdapter,
+		Returns:  runtime.Value{Type: types.Float},
+	}
+	addFloatInteger := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: addAdapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	addInteger := runtime.Signature{
 		Expected: []runtime.Value{
@@ -259,25 +389,32 @@ func loadAddition(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			return runtime.Value{
-				Type: types.Integer,
-				Data: a.Data.(int64) + b.Data.(int64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Integer},
+		Function: addAdapter,
+		Returns:  runtime.Value{Type: types.Integer},
+	}
+	addIntegerFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: addAdapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	addFunction := runtime.Function{
 		Signatures: []runtime.Signature{
 			joinString,
 			addInteger,
+			addIntegerFloat,
 			addFloat,
+			addFloatInteger,
 			plusFloat,
 			plusInteger,
 		},
@@ -331,6 +468,39 @@ func loadSubtraction(c *runtime.Context) {
 		}),
 		Returns: runtime.Value{Type: types.Integer},
 	}
+	subAdapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
+		var (
+			identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
+			identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
+			a         = identA.(runtime.Value)
+			b         = identB.(runtime.Value)
+			err       error
+		)
+		if a.Type == types.Float {
+			b, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		} else if b.Type == types.Float {
+			a, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		}
+		switch a.Type {
+		case types.Integer:
+			return runtime.Value{
+				Type: types.Integer,
+				Data: a.Data.(int64) - b.Data.(int64),
+			}, nil
+		case types.Float:
+			return runtime.Value{
+				Type: types.Float,
+				Data: a.Data.(float64) - b.Data.(float64),
+			}, nil
+		}
+		return runtime.Value{}, runtime.NewNotApplicableException("/", a.Type, b.Type)
+	})
 	subFloat := runtime.Signature{
 		Expected: []runtime.Value{
 			{
@@ -344,19 +514,24 @@ func loadSubtraction(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			return runtime.Value{
-				Type: types.Float,
-				Data: a.Data.(float64) - b.Data.(float64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Float},
+		Function: subAdapter,
+		Returns:  runtime.Value{Type: types.Float},
+	}
+	subFloatInteger := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: subAdapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	subInteger := runtime.Signature{
 		Expected: []runtime.Value{
@@ -371,24 +546,31 @@ func loadSubtraction(c *runtime.Context) {
 				Constant: true,
 			},
 		},
-		Function: nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
-			var (
-				identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
-				identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
-				a         = identA.(runtime.Value)
-				b         = identB.(runtime.Value)
-			)
-			return runtime.Value{
-				Type: types.Integer,
-				Data: a.Data.(int64) - b.Data.(int64),
-			}, nil
-		}),
-		Returns: runtime.Value{Type: types.Integer},
+		Function: subAdapter,
+		Returns:  runtime.Value{Type: types.Integer},
+	}
+	subIntegerFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: subAdapter,
+		Returns:  runtime.Value{Type: types.Float},
 	}
 	subFunction := runtime.Function{
 		Signatures: []runtime.Signature{
 			subInteger,
+			subIntegerFloat,
 			subFloat,
+			subFloatInteger,
 			minusFloat,
 			minusInteger,
 		},
