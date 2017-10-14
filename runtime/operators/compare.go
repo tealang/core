3567285eec4fbf83c2a090bc73447a0d40ff10a7
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-14 15:22:52 +0200

    Add greater equal and smaller equal operators

--- runtime/operators/compare.go
+++ runtime/operators/compare.go
@@ -6,6 +6,236 @@ import (
 	"github.com/tealang/tea-go/runtime/types"
 )
 
+func loadGreaterEqual(c *runtime.Context) {
+	adapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
+		var (
+			identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
+			identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
+			a         = identA.(runtime.Value)
+			b         = identB.(runtime.Value)
+			err       error
+		)
+		if a.Type == types.Float {
+			b, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		} else if b.Type == types.Float {
+			a, err = b.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		}
+		switch a.Type {
+		case types.Integer:
+			return runtime.Value{
+				Type: types.Bool,
+				Data: a.Data.(int64) >= b.Data.(int64),
+			}, nil
+		case types.Float:
+			return runtime.Value{
+				Type: types.Bool,
+				Data: a.Data.(float64) >= b.Data.(float64),
+			}, nil
+		}
+		return runtime.Value{}, runtime.NewNotApplicableException("<", a.Type, b.Type)
+	})
+	greaterEqualFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	greaterEqualFloatInt := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	greateEqualInt := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	greaterEqualIntFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	greaterEqualFunction := runtime.Function{
+		Signatures: []runtime.Signature{
+			greaterEqualFloat,
+			greaterEqualFloatInt,
+			greateEqualInt,
+			greaterEqualIntFloat,
+		},
+		Source: nil,
+	}
+	greaterEqual := runtime.Operator{
+		Function: greaterEqualFunction,
+		Symbol:   ">=",
+		Constant: true,
+	}
+	c.Namespace.Store(greaterEqual)
+}
+
+func loadSmallerEqual(c *runtime.Context) {
+	adapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
+		var (
+			identA, _ = c.Namespace.Find(runtime.SearchIdentifier, "a")
+			identB, _ = c.Namespace.Find(runtime.SearchIdentifier, "b")
+			a         = identA.(runtime.Value)
+			b         = identB.(runtime.Value)
+			err       error
+		)
+		if a.Type == types.Float {
+			b, err = a.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		} else if b.Type == types.Float {
+			a, err = b.Type.Cast(b)
+			if err != nil {
+				return runtime.Value{}, err
+			}
+		}
+		switch a.Type {
+		case types.Integer:
+			return runtime.Value{
+				Type: types.Bool,
+				Data: a.Data.(int64) <= b.Data.(int64),
+			}, nil
+		case types.Float:
+			return runtime.Value{
+				Type: types.Bool,
+				Data: a.Data.(float64) <= b.Data.(float64),
+			}, nil
+		}
+		return runtime.Value{}, runtime.NewNotApplicableException("<", a.Type, b.Type)
+	})
+	smallerEqualFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	smallerEqualFloatInt := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Float,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	smallerEqualInt := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Integer,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	smallerEqualIntFloat := runtime.Signature{
+		Expected: []runtime.Value{
+			{
+				Name:     "a",
+				Type:     types.Integer,
+				Constant: true,
+			},
+			{
+				Name:     "b",
+				Type:     types.Float,
+				Constant: true,
+			},
+		},
+		Function: adapter,
+		Returns:  runtime.Value{Type: types.Bool},
+	}
+	smallerEqualFunction := runtime.Function{
+		Signatures: []runtime.Signature{
+			smallerEqualInt,
+			smallerEqualIntFloat,
+			smallerEqualFloat,
+			smallerEqualFloatInt,
+		},
+		Source: nil,
+	}
+	smallerEqual := runtime.Operator{
+		Function: smallerEqualFunction,
+		Symbol:   "<=",
+		Constant: true,
+	}
+	c.Namespace.Store(smallerEqual)
+}
+
 func loadGreater(c *runtime.Context) {
 	adapter := nodes.NewAdapter(func(c *runtime.Context) (runtime.Value, error) {
 		var (
@@ -238,5 +468,7 @@ func loadSmaller(c *runtime.Context) {
 
 func LoadCompare(c *runtime.Context) {
 	loadSmaller(c)
+	loadSmallerEqual(c)
 	loadGreater(c)
+	loadGreaterEqual(c)
 }
