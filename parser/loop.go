f2a4ea8b1bf7595373de9304344537463b07e78b
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-19 21:50:55 +0200

    Add loop parser

--- parser/loop.go
+++ parser/loop.go
@@ -6,8 +6,52 @@ import (
 )
 
 type loopParser struct {
+	index, size int
 }
 
 func (lp *loopParser) Parse(input []tokens.Token) (nodes.Node, int, error) {
-	return nil, 0, nil
+	lp.index, lp.size = 1, len(input)
+
+	entry, n, err := newTermParser().Parse(input[lp.index:])
+	if err != nil {
+		return nil, lp.index, err
+	}
+	// check if single-tier loop head
+	if input[lp.index+n].Type == tokens.LeftBlock {
+		lp.index += n
+		body, n, err := newSequenceParser(false, 0).Parse(input[lp.index+1:])
+		if err != nil {
+			return nil, lp.index, err
+		}
+		// ignore right block
+		lp.index += n + 2
+		return nodes.NewLoop(entry, body), lp.index, nil
+	}
+
+	// handle three-tier loop
+	sequ, n, err := newSequenceParser(false, 3).Parse(input[lp.index:])
+	if err != nil {
+		return nil, lp.index, err
+	}
+	head := sequ.(*nodes.Sequence)
+	lp.index += n
+
+	if input[lp.index].Type != tokens.LeftBlock {
+		return nil, lp.index, newUnexpectedTokenException(input[lp.index].Type)
+	}
+	if len(head.Childs) != 3 {
+		return nil, lp.index, newParseException("Expected three-tier for-loop")
+	}
+
+	body, n, err := newSequenceParser(false, 0).Parse(input[lp.index+1:])
+	if err != nil {
+		return nil, lp.index, err
+	}
+	lp.index += n + 2
+
+	return nodes.NewSequence(true, head.Childs[0], nodes.NewLoop(head.Childs[1], nodes.NewSequence(false, body, head.Childs[2]))), lp.index, nil
+}
+
+func newLoopParser() *loopParser {
+	return &loopParser{}
 }
