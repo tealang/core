4e9966bc3f3e822f68aa1f9548f50c18a1df14ca
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-19 22:38:44 +0200

    Add function literal node

--- runtime/nodes/function.go
+++ runtime/nodes/function.go
@@ -51,3 +51,44 @@ func NewFunctionCall(alias string, args ...Node) *FunctionCall {
 	call.Metadata["label"] = fmt.Sprintf("Call (name='%s')", alias)
 	return call
 }
+
+type FunctionLiteral struct {
+	BasicNode
+	Args    []*Typecast
+	Returns *Typecast
+}
+
+func (literal *FunctionLiteral) Eval(c *runtime.Context) (runtime.Value, error) {
+	// load arg types
+	args := make([]runtime.Value, len(literal.Childs)-1)
+	for i, arg := range literal.Args {
+		value, err := arg.Eval(c)
+		if err != nil {
+			return runtime.Value{}, err
+		}
+		args[i] = value
+	}
+	returns := runtime.Value{}
+	// load return types
+	if literal.Returns != nil {
+		value, err := literal.Returns.Eval(c)
+		if err != nil {
+			return runtime.Value{}, err
+		}
+		returns = value
+	}
+	signature := runtime.NewSignature(returns, literal.Childs[0], args)
+	function := runtime.NewFunction(c.Namespace, signature)
+	return runtime.Value{
+		Type: types.Function,
+		Data: function,
+	}, nil
+}
+
+func NewFunctionLiteral(body, returns *Typecast, args ...*Typecast) *FunctionLiteral {
+	return &FunctionLiteral{
+		BasicNode: NewBasic(body),
+		Returns:   returns,
+		Args:      args,
+	}
+}
