1e4a1af7e51e18b9c1b436b8f8d2859618446a78
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-21 20:36:26 +0200

    Add operator definition node

--- runtime/nodes/function.go
+++ runtime/nodes/function.go
@@ -63,13 +63,13 @@ type FunctionLiteral struct {
 	Returns *Typecast
 }
 
-func (literal *FunctionLiteral) Eval(c *runtime.Context) (runtime.Value, error) {
+func (literal *FunctionLiteral) buildSignature(c *runtime.Context) (runtime.Signature, error) {
 	// load arg types
 	args := make([]runtime.Value, len(literal.Args))
 	for i, arg := range literal.Args {
 		value, err := arg.Eval(c)
 		if err != nil {
-			return runtime.Value{}, err
+			return runtime.Signature{}, err
 		}
 		args[i] = value
 	}
@@ -78,11 +78,19 @@ func (literal *FunctionLiteral) Eval(c *runtime.Context) (runtime.Value, error) 
 	if literal.Returns != nil {
 		value, err := literal.Returns.Eval(c)
 		if err != nil {
-			return runtime.Value{}, err
+			return runtime.Signature{}, err
 		}
 		returns = value
 	}
 	signature := runtime.NewSignature(returns, literal.Childs[0], args)
+	return signature, nil
+}
+
+func (literal *FunctionLiteral) Eval(c *runtime.Context) (runtime.Value, error) {
+	signature, err := literal.buildSignature(c)
+	if err != nil {
+		return runtime.Value{}, err
+	}
 	function := runtime.NewFunction(c.Namespace, signature)
 	return runtime.Value{
 		Type: types.Function,
@@ -90,7 +98,7 @@ func (literal *FunctionLiteral) Eval(c *runtime.Context) (runtime.Value, error) 
 	}, nil
 }
 
-func (Literal *FunctionLiteral) Name() string {
+func (FunctionLiteral) Name() string {
 	return "FunctionLiteral"
 }
 
@@ -110,4 +118,54 @@ func NewFunctionLiteral(body Node, returns *Typecast, args ...*Typecast) *Functi
 		lit.Metadata["label"] = fmt.Sprintf("Function <%s>", types)
 	}
 	return lit
+}
+
+type OperatorDefinition struct {
+	FunctionLiteral
+	Symbol string
+}
+
+func (OperatorDefinition) Name() string {
+	return "OperatorDefinition"
+}
+
+func (definition *OperatorDefinition) Eval(c *runtime.Context) (runtime.Value, error) {
+	signature, err := definition.buildSignature(c)
+	if err != nil {
+		return runtime.Value{}, err
+	}
+	function := runtime.NewFunction(c.Namespace, signature)
+	operator := runtime.Operator{
+		Function: function,
+		Symbol:   definition.Symbol,
+		Constant: true,
+	}
+	if err := c.Namespace.Store(operator); err != nil {
+		return runtime.Value{}, err
+	}
+	return runtime.Value{
+		Type: types.Function,
+		Data: function,
+	}, nil
+}
+
+func NewOperatorDefinition(symbol string, body Node, returns *Typecast, args ...*Typecast) *OperatorDefinition {
+	def := &OperatorDefinition{
+		FunctionLiteral: FunctionLiteral{
+			BasicNode: NewBasic(body),
+			Returns:   returns,
+			Args:      args,
+		},
+		Symbol: symbol,
+	}
+	types := make([]string, len(args))
+	for i, a := range args {
+		types[i] = a.Alias
+	}
+	if returns != nil {
+		def.Metadata["label"] = fmt.Sprintf("Define %s as <%s> -> %s", symbol, types, returns.Alias)
+	} else {
+		def.Metadata["label"] = fmt.Sprintf("Define %s as <%s> -> ()", symbol, types)
+	}
+	return def
 }
