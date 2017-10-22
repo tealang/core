e09d33ce8c5e60995dbd45b99d6816d03ba68406
Lennart Espe <lnsp@users.noreply.github.com>
2017-10-22 17:40:14 +0200

    Improve tea command line tool

--- cmd/tea/main.go
+++ cmd/tea/main.go
@@ -3,49 +3,24 @@ package main
 
 import (
 	"bufio"
-	"flag"
+	"errors"
 	"fmt"
 	"os"
 	"strings"
 
 	"github.com/tealang/core/repl"
-)
-
-const (
-	welcomeText = `Tealang v0.1.0-alpha
-Copyright 2017 Lennart Espe. All rights reserved.`
-	replSymbol = "> "
+	"gopkg.in/urfave/cli.v1"
 )
 
 var (
 	interactiveMode, graphvizMode bool
 )
 
-func main() {
-	flag.BoolVar(&interactiveMode, "i", false, "Start in interactive mode")
-	flag.BoolVar(&graphvizMode, "g", false, "Enable GraphViz visualization mode")
-	flag.Parse()
-
-	env := repl.New(repl.Config{
-		OutputGraph: graphvizMode,
-	})
-	if filename := flag.Arg(0); filename != "" {
-		err := env.Load(filename)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-	} else {
-		interactiveMode = true
-		fmt.Fprintln(os.Stdout, welcomeText)
-	}
-
-	if !interactiveMode {
-		return
-	}
-
+func runInteractiveShell(c *cli.Context) error {
+	env := repl.New(repl.Config{OutputGraph: c.GlobalBool("graph")})
 	reader := bufio.NewReader(os.Stdin)
 	for env.Active {
-		fmt.Fprint(os.Stdout, replSymbol)
+		fmt.Fprint(os.Stdout, "> ")
 		input, err := reader.ReadString('\n')
 		if err != nil {
 			env.Stop()
@@ -59,4 +34,41 @@ func main() {
 		}
 	}
 	fmt.Fprintln(os.Stdout)
+	return nil
+}
+
+func executeProgramFile(c *cli.Context) error {
+	if c.NArg() < 1 {
+		return errors.New("required filename")
+	}
+	return repl.New(repl.Config{OutputGraph: c.GlobalBool("graph")}).Load(c.Args()[0])
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "tea"
+	app.Usage = "interactive runtime environment"
+	app.Copyright = "2017 Lennart Espe. All rights reserved."
+	app.Author = "Lennart Espe <lennart@espe.tech>"
+	app.Version = "v0.1.0-dev"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:   "graph",
+			Usage:  "Show graphviz output instead of program result",
+			Hidden: false,
+		},
+	}
+	app.Commands = []cli.Command{
+		{
+			Name:   "shell",
+			Usage:  "Run an interactive interpreter",
+			Action: runInteractiveShell,
+		},
+		{
+			Name:   "run",
+			Usage:  "Execute a program file",
+			Action: executeProgramFile,
+		},
+	}
+	app.Run(os.Args)
 }
